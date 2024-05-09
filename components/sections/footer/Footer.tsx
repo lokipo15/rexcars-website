e8d73e866aef94c747df816532e1d59ffb473a28
Gabriel Boruń <borun.gabriel@gmail.com>
2024-05-09 23:46:14 +0200

    REFACTOR: removed "regulamin" and "Opłaty" links from the footer

--- components/sections/footer/Footer.tsx
+++ components/sections/footer/Footer.tsx
@@ -9,13 +9,13 @@ export default function Footer() {
             <span className="md:text-left text-center">
                 {copyright} {year} RexCars | Wszystkie prawa zastrzeżone.
             </span>
+
             <Link className="hover:text-blue-primary md:text-left text-center ease-in-out duration-300 md:py-0 py-1" href="/polityka-prywatnosci">Polityka Prywatności</Link>
-            <Link className="hover:text-blue-primary md:text-left text-center ease-in-out duration-300 md:py-0 py-1" href="/regulamin">Regulamin</Link>
-            <Link className="hover:text-blue-primary md:text-left text-center ease-in-out duration-300 md:py-0 py-1" href="/tabela-oplat">Opłaty</Link>
+            
             <span className='flex flex-row md:py-0 py-1 md:text-left justify-center'>
                 Coded with ❤️ by{' '}
                 <p className='font-medium text-blue-500 ps-1'> {"<"}CloudBurst {"/>"}</p>
             </span>
         </footer>
-    );
+    );  
 }
