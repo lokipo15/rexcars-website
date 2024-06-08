7a86bff81cff3807204a53ade6226ad04d9f9e8c
Gabriel Boruń <borun.gabriel@gmail.com>
2024-06-08 20:14:09 +0200

    FIX: Fixed error with using "useSearchParams()" outside of <Suspense> boundary

--- components/sections/nav/Navbar.tsx
+++ components/sections/nav/Navbar.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import NavbarItemList from './NavbarItemList';
 import Modal from '@/components/ui/modal/Modal';
 import MobileNavbarItemList from './MobileNavbarItemList';
+import { Suspense } from 'react';
 
 export default function Navbar() {
     return (
@@ -22,7 +23,9 @@ export default function Navbar() {
             </Link>
             <NavbarItemList />
             <MobileNavbarItemList />
-            <Modal />
+            <Suspense>
+                <Modal />
+            </Suspense>
         </nav>
     );
 }
