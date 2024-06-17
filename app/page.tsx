dd7fbbc8500617b0bc665ef252cbf16d7fc8aed7
Gabriel Boruń <borun.gabriel@gmail.com>
2024-06-17 21:10:47 +0200

    FIX: Added Metadata to home page.

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,29 @@ import HeroBanner from "@/components/sections/homepage/hero-banner/HeroBanner";
 import OurCarsYourExperiences from "@/components/sections/homepage/our-cars-your-experiences/OurCarsYourExperiences";
 import WhyUs from "@/components/sections/homepage/why-us/WhyUs";
 import YourRaceBeginsNow from "@/components/sections/homepage/your-race-begins-now/YourRaceBeginsNow";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Wypożyczalnia samochodów sportowych | RexCars",
+    description: "Wypożyczalnia samochodów sportowych z dowozem na całą Polskę. Szeroka gama dostępnych aut sportowych na wynajem. Spełnij swoje motoryzacyjne marzenia już dziś!",
+    robots: "index, follow",
+    openGraph: {
+        title: "Wypożyczalnia samochodów sportowych | RexCars",
+        description: "Wypożyczalnia samochodów sportowych z dowozem na całą Polskę. Szeroka gama dostępnych aut sportowych na wynajem. Spełnij swoje motoryzacyjne marzenia już dziś!",
+        type: "website",
+        siteName: "Wypożyczalnia samochodów sportowych | RexCars",
+        locale: "pl/PL",
+        images: [
+            {
+                url: "https://cdn.sanity.io/images/pfrvgfvt/production/f5d944de5a4800c6a7b69848e7a9f8c04b4ac9c9-5732x3224.jpg?h=675",
+                width: 1200,
+                height: 675,
+                alt: "Samochody sportowe dostępne do wynajęcia w wypożyczalni RexCars",
+            },
+        ],
+    }
+
+};
 
 export default function Home() {
     return (
